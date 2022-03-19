796d81316466622599508417ff5d54f0fbad7f61
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-03-19 13:44:32 +0530

    Modified IcyText

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,34 +3,46 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-// Craete a User using: POST "/api/auth/", Doesn't require Auth
-router.post('/',[
+ // Create a User using: POST "/api/auth/createuser", No login required
+router.post('/createuser',[
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], (req, res)=>{
+], async (req, res)=>{
+
+ // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+ // Check whether the user with this email exists already
+ try{
+
+
+
+    let user = await User.findOne({email: req.body.email});
+    // console.log(user)
+    if(user){
+      return res.status(400).json({error: "Sorry! a user with this email already exists"})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err)
-    res.json({error: 'Please enter a unique value for email'})})
+      })
+      
+      res.json(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured")
+    }
 
-      res.send(req.body);
-    // console.log(req.body);
-    // const user = User(req.body);
-    // user.save()
-    
-    // obj= {
-        //     a: 'Trinity',
-        //     number: 54
-        // }
-        // res.json(obj)
+
+
+      // .then(user => res.json(user))
+      // .catch(err=> {console.log(err)
+    // res.json({error: 'Please enter a unique value for email', message: err.message})})
 })
 
 module.exports = router
