e229e02949cce7e67d5e2e5aaab8b5431106496c
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-03-20 15:48:53 +0530

    MOdified IcyText

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,7 +4,7 @@ const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
 var fetchuser = require("../middleware/fetchuser");
 
-// ROUTE1: Get All the Notes using: GET "/api/auth/createuser", No login required
+// ROUTE1: Get All the Notes using: GET "/api/auth/createuser", login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
     try{
   const notes = await Note.find({ user: req.user.id });
@@ -15,7 +15,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
     }
 });
 
-// ROUTE2: Add a new Note using: POST "/api/auth/addnote", No login required
+// ROUTE2: Add a new Note using: POST "/api/auth/addnote", login required
 router.post("/addnote", fetchuser, [
     body("title", "Enter a valid title").isLength({ min: 3 }),
     body("description", "Description must be atleast 5 characters").isLength({min: 5,}),
@@ -42,4 +42,28 @@ router.post("/addnote", fetchuser, [
 }
 })
 
+// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        // Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
 module.exports = router;
